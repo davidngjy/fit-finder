f43f63b92b0748b455be16c5f3c6465644a799e9
davidngjy <jianyongng@gmail.com>
2020-09-12 17:53:02 +1000

    Add google login

---
+++ app/src/main/java/com/sample/fitfinder/LoginActivity.kt
@@ -0,0 +1,94 @@
+package com.sample.fitfinder
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.card.MaterialCardView
+import com.sample.fitfinder.databinding.ActivityLoginBinding
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var progressBarCard: MaterialCardView
+    private lateinit var signInButton: SignInButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        progressBarCard = binding.progressBarCard
+        signInButton = binding.signInButton
+        setupSignInButton()
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestId()
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null) navigateToMainActivity()
+    }
+
+    private fun setupSignInButton() {
+        signInButton.apply {
+            setSize(SignInButton.SIZE_WIDE)
+            setOnClickListener {
+                signInButton.visibility = View.INVISIBLE
+                progressBarCard.visibility = View.VISIBLE
+                val signInIntent = googleSignInClient.signInIntent
+                startActivityForResult(signInIntent, RC_SIGN_IN)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        signInButton.visibility = View.VISIBLE
+        progressBarCard.visibility = View.INVISIBLE
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN && resultCode == -1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            navigateToMainActivity()
+        } catch (e: ApiException) {
+            if (e.statusCode != 4) binding.error.text = e.statusCode.toString()
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val RC_SIGN_IN = 1234
+    }
+}
--- app/src/main/java/com/sample/fitfinder/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/sample/fitfinder/ui/profile/ProfileFragment.kt
@@ -1,11 +1,15 @@
 package com.sample.fitfinder.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.sample.fitfinder.LoginActivity
 import com.sample.fitfinder.data.repository.UserRepository
 import com.sample.fitfinder.databinding.FragmentProfileBinding
 import com.sample.fitfinder.ui.profile.viewmodel.ProfileViewModel
@@ -14,6 +18,7 @@ import com.sample.fitfinder.ui.profile.viewmodel.ProfileViewModelFactory
 class ProfileFragment : Fragment() {
 
     private val profileViewModel: ProfileViewModel by viewModels { ProfileViewModelFactory(UserRepository()) }
+
     private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
@@ -23,8 +28,20 @@ class ProfileFragment : Fragment() {
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
+        binding.signOutButton.setOnClickListener { onSignOut() }
+
         binding.profileViewModel = profileViewModel
 
         return binding.root
+    }
+
+    private fun onSignOut() {
+        GoogleSignIn
+            .getClient(requireContext(), GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .signOut()
+
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
